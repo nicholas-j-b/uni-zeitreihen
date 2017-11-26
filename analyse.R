32e723ca9fd617d2ed0357b13b93efdc9cd94f75
nicholas-j-b <nicholas.j.brooking@gmail.com>
2017-11-26 14:09:51 +0100

    finished

--- analyse.R
+++ analyse.R
@@ -1,50 +1,34 @@
 
-setwd("Z:/zeitreihen/stoxx50e")
+#setwd("Z:/zeitreihen/stoxx50e")
+setwd("~/r-ws/zeitreihen/proj1/uni-zeitreihen")
+
 
 
 #1Date 2Open 3High 4Low 5Close* 6Adj 7Close** 8Volume
 H <- read.csv("^STOXX50E.csv")
-
+a <- 1558
+b <- 7928
+n <- b - a + 1
 t <- as.Date(H[,1])
-y <- H[,2]
-y <- c(y[1:7928])
-y.2 <- ifelse(y == 7676, NaN, y)
-
-y.3 <- y.2[a:b]
-
-# for(i in 1:(length(y))){
-# 	if(y[i] == 7676){
-# 		y.3 <- c(y.3, y[i - 1])
-# 	}else{
-# 		y.3 <- c(y.3, y)
-# 	}
-# }
-# 
-# for(i in 1:7928){
-# 	if(y[i] == 7676){
-# 		y.3 <- c(y.3, y[i - 1])
-# 	}else{
-# 		y.3 <- c(y.3, y)
-# 	}
-# }
+y <- H[,6]
+y <- c(y[a:b])
+t <- c(t[a:b])
+y <- ifelse(y == 7678, NaN, y)
+r <- y[2:n] - y[1:(n - 1)]
 
 
 
-y.log <- log(y)
 
-y.lm <- lm(y.log~t)
 
-layout(1:2)
+#plot(t, y, type = "l")
 
-plot(t, y, type = "l")
 
-q <- ifelse(y > 7200, 0, y)
-plot(t, y.2, type = "l")
-
-# plot(t[a:b], y.3, type = "l")
-# 
-# a <- 700
-# b <- a + 50
-# y.2[a:b]
+par(mfrow=c(2,2),mar=c(2,2,1,1)) 
+plot(t,y,type="l"); mtext("(a)",side=3,cex=0.7)  
+plot(t[2:n],r,type="l"); mtext("(b)",side=3,cex=0.7)  
+plot(t[2:n],r^2,type="l"); 
+mtext("(c)",side=3,cex=0.7)  
+plot(t[2:n],r^4,type="l"); 
+mtext("(d)",side=3,cex=0.7)  
 
 
